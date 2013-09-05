a2db79535a46770935f1744349040ad320527835
Corwin Mcknight <lioneltabre@gmail.com>
2013-09-04 23:43:32 -0700

    Create token.h

---
+++ includes/token.h
@@ -0,0 +1,14 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+class Token
+{
+	int id;
+	string rule;
+	string descriptor;
+	string descriptor_params[2]; //Maximum of THREE Paramaters.
+	string action;
+	//TokenAnalasys ta;
+}
+
+#endif
