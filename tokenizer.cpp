0b73c7402f7d71085b3659fd2a9a51a809c43c15
Corwin Mcknight <lioneltabre@gmail.com>
2013-09-05 00:54:38 -0700

    Update tokenizer.cpp

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,13 +1,38 @@
 #include "includes/token.h"
 #include "includes/tokenizer.h"
-
-
+#include <vector>
+#include <string>
+#include <stdio.h>
 using namespace std;
 
+vector<string> split(const char *str, char c = ' ')
+{
+    vector<string> result;
+
+    do
+    {
+        const char *begin = str;
+
+        while(*str != c && *str)
+            str++;
+
+        result.push_back(string(begin, str));
+    } while (0 != *str++);
+
+    return result;
+}
 
 Token Tokenizer::convert_to_token(string line)
 {
 	Token result;
+	vector<string> tokens;
+	tokens = split(line,',')
+	for (vector<string>::iterator n = tokens.begin();
+                              n != tokens.end();
+                              ++n)
+	{
+	    printf("Token:%s\n",*n);
+	}
 	
 	return result;
 }
