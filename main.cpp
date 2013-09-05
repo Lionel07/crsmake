8c3c58209e42d471946bc773b92823e4408e6120
Corwin Mcknight <lioneltabre@gmail.com>
2013-09-05 00:13:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,29 @@
 // main.cpp
 //
 
+//Includes
+#include <iostream>
+#include <fstream>
+#include <stdio>
+using namespace std;
+
+//Variables
+ifstream rulefile;
+string line;
+int lineno=0;
 int main()
 {
+	printf("crsmake - v. 0.1");
+	rulefile = new ifstream("crsmake.csv");
+	//Just echo for now.
+	if (rulefile.is_open())
+	{
+		while ( rulefile.good() )
+		{
+			getline (rulefile,line);
+			printf("[%d]\t:%s\n",lineno,line.c_str());
+			lineno++;
+		}
+		myfile.close();
+	}
 }
